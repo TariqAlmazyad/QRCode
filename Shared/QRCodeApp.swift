e55c7b379138d9b09a774fc4cfa55b7bda1b2fa0
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2021-03-06 09:11:04 +0300

    Initial Commit

---
+++ Shared/QRCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeApp.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
